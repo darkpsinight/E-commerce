722474e1f78e682cedeaf53f09a7c956f91b51bc
darkpsinight <darkpsinight@hotmail.com>
2022-07-02 16:20:41 +0100

    2/7/2022

--- front/src/components/Slider.jsx
+++ front/src/components/Slider.jsx
@@ -1,12 +1,14 @@
 import { ArrowLeftOutlined, ArrowRightOutlined } from "@material-ui/icons";
+import { useState } from "react";
 import styled from "styled-components";
+import { sliderItems } from "../data";
 
 const Container = styled.div`
   width: 100%;
   height: 100vh;
   display: flex;
-  background-color: coral;
   position: relative;
+  overflow: hidden;
 `;
 
 const Arrow = styled.div`
@@ -22,12 +24,17 @@ const Arrow = styled.div`
   bottom: 0;
   left: ${(props) => props.direction === "left" && "10px"};
   right: ${(props) => props.direction === "right" && "10px"};
-  cursor: pointer;
   margin: auto;
+  cursor: pointer;
+  opacity: 0.5;
+  z-index: 2;
 `;
 
 const Wrapper = styled.div`
   height: 100%;
+  display: flex;
+  transition: all 1.5s ease;
+  transform: translateX(${(props) => props.slideIndex * -100}vw);
 `;
 
 const Slide = styled.div`
@@ -35,6 +42,7 @@ const Slide = styled.div`
   height: 100vh;
   display: flex;
   align-items: center;
+  background-color: #${(props) => props.bg};
 `;
 
 const ImgContainer = styled.div`
@@ -51,37 +59,55 @@ const InfoContainer = styled.div`
   padding: 50px;
 `;
 
-const Title = styled.div`
-  flex: 1;
-  padding: 50px;
+const Title = styled.h1`
+  font-size: 70px;
 `;
 
-const Description = styled.div`
-  flex: 1;
-  padding: 50px;
+const Desc = styled.p`
+  margin: 50px 0px;
+  font-size: 20px;
+  font-weight: 500;
+  letter-spacing: 3px;
 `;
 
-const Button = styled.div`
-  flex: 1;
-  padding: 50px;
+const Button = styled.button`
+  padding: 10px;
+  font-size: 20px;
+  background-color: transparent;
+  cursor: pointer;
 `;
 
 const Slider = () => {
+  const [slideIndex, setslideIndex] = useState(0);
+  const handleClick = (direction) => {
+    if (direction === "left") {
+      setslideIndex(slideIndex > 0 ? slideIndex - 1 : 2);
+    } else {
+      setslideIndex(slideIndex < 2 ? slideIndex + 1 : 0);
+    }
+  };
+
   return (
     <div>
       <Container>
-        <Arrow direction="left">
+        <Arrow direction="left" onClick={() => handleClick("left")}>
           <ArrowLeftOutlined />
         </Arrow>
-        <Wrapper>
-          <Slide>
-            <ImgContainer>
-              <Image src="https://i.ibb.co/LYRx42N/5.jpg" />
-            </ImgContainer>
-            <InfoContainer></InfoContainer>
-          </Slide>
+        <Wrapper slideIndex={slideIndex}>
+          {sliderItems.map((item) => (
+            <Slide bg={item.bg}>
+              <ImgContainer>
+                <Image src={item.img} />
+              </ImgContainer>
+              <InfoContainer>
+                <Title>{item.title}</Title>
+                <Desc>{item.desc}</Desc>
+                <Button>SHOW NOW</Button>
+              </InfoContainer>
+            </Slide>
+          ))}
         </Wrapper>
-        <Arrow direction="right">
+        <Arrow direction="right" onClick={() => handleClick("right")}>
           <ArrowRightOutlined />
         </Arrow>
       </Container>
---
+++ front/src/data.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+    {
+        id: 1,
+        img: "https://i.ibb.co/LYRx42N/1.jpg",
+        title: "SUMMER SALE",
+        desc: "DON'T COMPROISE ON STYLE! GET FLAT 30% OFF NEW ARRIVALS.",
+        bg: "f5fafd",
+    },
+    {
+        id: 2,
+        img: "https://i.ibb.co/PZTx1wX/2.jpg",
+        title: "WINTER COLLECTION",
+        desc: "DON'T COMPROISE ON STYLE! GET FLAT 30% OFF NEW ARRIVALS.",
+        bg: "fcf1ed",
+    },
+    {
+        id: 3,
+        img: "https://i.ibb.co/ZXRJyqR/3.jpg",
+        title: "POPULAR SALE",
+        desc: "DON'T COMPROISE ON STYLE! GET FLAT 30% OFF NEW ARRIVALS.",
+        bg: "fbf0f4",
+    }
+]
