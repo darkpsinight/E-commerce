9f0a8f21914056d3c87037b5ac2482980655bbd6
darkpsinight <darkpsinight@hotmail.com>
2022-07-04 20:11:37 +0100

    4/7/2022

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,11 +1,19 @@
 /* import Home from "./pages/Home"; 
 import ProductList from "./pages/ProductList"; 
 import Product from "./pages/Product";
-import Register from "./pages/Register";*/
-import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Login from "./pages/Login";*/
+import Cart from "./pages/Cart";
 
 const App = () => {
-  return /* <Home /> <ProductList /> <Product /> <Register />*/ <Login />;
+  return (
+    /* <Home />
+    <ProductList />
+    <Product />
+    <Register />
+    <Login />*/
+    <Cart />
+  );
 };
 
 export default App;
