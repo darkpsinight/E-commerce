d286f43ef32e27329d073d0fa96002ad61e85d91
darkpsinight <darkpsinight@hotmail.com>
2022-07-06 22:26:06 +0100

    6/4/2022

--- back/routes/auth.js
+++ back/routes/auth.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const User = require("../models/User")
 const CryptoJS = require("crypto-js")
+const jwt = require("jsonwebtoken")
 
 //REGISTER
 router.post("/register", async (req, res) => {
@@ -32,13 +33,25 @@ router.post("/login", async (req, res) => {
             user.password,
             process.env.PASS_SEC
         );
-        const password = hashedPassword.toString(CryptoJS.enc.Utf8)
-        password !== req.body.password && res.status(401).json("Wrong credentials!");
+        const OriginalPassword = hashedPassword.toString(CryptoJS.enc.Utf8)
+        OriginalPassword !== req.body.password && res.status(401).json("Wrong credentials!");
 
-        res.status(200).json(user);
+        //JWT
+        const accessToken = jwt.sign(
+            {
+                id: user._id,
+                isAdmin: user.isAdmin
+            },
+            process.env.JWT_SEC,
+            { expiresIn: "3d" }
+        );
 
-    } catch (error) {
-        res.status(500).json(error)
+        //_doc houwa el fichier illi MongoDB y'save fih data, nzidouh béch yrakka7 affichage, w hakka ywalli password mayit'la3ch f Postman
+        const { password, ...others } = user._doc;
+        res.status(200).json({ ...others, accessToken });
+
+    } catch (err) {
+        res.status(500).json(err)
     }
 });
 
