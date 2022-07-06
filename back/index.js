f796a9d7598356b4dd5e9c3b010efa7cc529f403
darkpsinight <darkpsinight@hotmail.com>
2022-07-06 13:06:52 +0100

    first creation of backendapi - 6/4/2022

---
+++ back/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose")
+const dotenv = require("dotenv")
+
+
+dotenv.config();
+mongoose
+    .connect(process.env.MONGO_URL)
+    .then(() => console.log("DB connection Successfull"))
+    .catch((err) => console.log(err))
+
+app.listen(process.env.PORT || 5000, () => {
+    console.log("Backend server is running on port 5000");
+})
