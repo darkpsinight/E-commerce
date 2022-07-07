af95ff0483b1b791621af30eaafd65a5d2fec6d2
darkpsinight <darkpsinight@hotmail.com>
2022-07-07 17:22:30 +0100

    7/7/2022

--- back/routes/user.js
+++ back/routes/user.js
@@ -1,5 +1,54 @@
 const router = require("express").Router();
+const User = require("../models/User");
+const { verifyToken, verifyTokenAndAuthorization, verifyTokenAndAdmin } = require("./verifyToken")
 
+//UPDATE
+router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
+    if (req.body.password) {
+        req.body.password = CryptoJS.AES.encrypt(
+            req.body.password,
+            process.env.PASS_SEC
+        ).toString();
+    }
+    try {
+        const updatedUser = await User.findByIdAndUpdate(req.params.id, {
+            $set: req.body
+        }, { new: true })
+        res.status(200).json(updatedUser);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
 
+//DELETE
+router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
+    try {
+        await User.findByIdAndDelete(req.params.id)
+        res.status(200).json("User has been deleted...");
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
 
-module.exports = router
+//GET USER (ONLY FOR ADMIN ROLE)
+router.get("/find/:id", verifyTokenAndAdmin, async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        const { password, ...others } = user._doc;
+        res.status(200).json({ others });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
+//GET ALL USERS + QUERY(ONLY FOR ADMIN ROLE)
+router.get("/", verifyTokenAndAdmin, async (req, res) => {
+    try {
+        const users = await User.find();
+        res.status(200).json({ users });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
+module.exports = router;
