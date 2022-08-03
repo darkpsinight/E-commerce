916dcb3279d81357aa48764fc3fdd41b16079a3b
darkpsinight <darkpsinight@hotmail.com>
2022-08-03 14:32:02 +0100

    3/8/2022- Firebase done

---
+++ admin/src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBh_7Prrc7Tqg0sYS8LuIowTUxMchu4mMY",
+    authDomain: "shoptn-73d1c.firebaseapp.com",
+    projectId: "shoptn-73d1c",
+    storageBucket: "shoptn-73d1c.appspot.com",
+    messagingSenderId: "100640857201",
+    appId: "1:100640857201:web:7b49b1af2a1dbdbdb08fd0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
--- admin/src/redux/apiCalls.js
+++ admin/src/redux/apiCalls.js
@@ -63,7 +63,7 @@ export const updateProduct = async (id, product, dispatch) => {
 export const addProduct = async (product, dispatch) => {
     dispatch(addProductStart());
     try {
-        const res = await userRequest.post(`/products`, { product })
+        const res = await userRequest.post(`/products/add`, product)
         dispatch(addProductSuccess(res.data))
     } catch (err) {
         dispatch(addProductFailure())
